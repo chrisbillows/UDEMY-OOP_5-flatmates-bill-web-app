657dc1a10d42f0248bf55148ec5a14d601f46f69
Chris Billows <chrisbillows@Chriss-MacBook-Pro.local>
2023-03-01 10:44:23 +0000

    Have results appear on bill form page
    Move results from from results.html to bill_form_page.html. Add variables to BillFormPage post method. Add conditional to results widget. Comment out ResultsPage and associated.

--- main.py
+++ main.py
@@ -19,9 +19,6 @@ class BillFormPage(MethodView):
         bill_form = BillForm()
         return render_template('bill_form_page.html', billform=bill_form)
 
-
-class ResultsPage(MethodView):
-
     def post(self):
         billform = BillForm(request.form)
 
@@ -29,11 +26,31 @@ class ResultsPage(MethodView):
         flatmate1 = flat.Flatmate(billform.name1.data, float(billform.days_in_house1.data))
         flatmate2 = flat.Flatmate(billform.name2.data, float(billform.days_in_house2.data))
 
-        return render_template('results.html',
-                               name1=flatmate1.name,
+        return render_template('bill_form_page.html',
+                               result=True,                 # triggers conditional
+                               billform=billform,           # need to fix inconsistent naming
+                               name1=flatmate1.name,        # needed as post request has no access to billform (could that be a class variable?)
                                amount1=flatmate1.pays(the_bill, flatmate2),
                                name2=flatmate2.name,
                                amount2=flatmate2.pays(the_bill, flatmate1))
+
+
+# Results now post to the bill_form_page
+#
+# class ResultsPage(MethodView):
+#
+#     def post(self):
+#         billform = BillForm(request.form)
+#
+#         the_bill = flat.Bill(float(billform.amount.data), billform.period.data)
+#         flatmate1 = flat.Flatmate(billform.name1.data, float(billform.days_in_house1.data))
+#         flatmate2 = flat.Flatmate(billform.name2.data, float(billform.days_in_house2.data))
+#
+#         return render_template('results.html',
+#                                name1=flatmate1.name,
+#                                amount1=flatmate1.pays(the_bill, flatmate2),
+#                                name2=flatmate2.name,
+#                                amount2=flatmate2.pays(the_bill, flatmate1))
 
 
 class BillForm(Form):
@@ -49,7 +66,7 @@ class BillForm(Form):
     button = SubmitField("Calculate")
 
 app.add_url_rule('/', view_func=HomePage.as_view('home_page'))
-app.add_url_rule('/bill', view_func=BillFormPage.as_view('bill_form_page'))
-app.add_url_rule('/results', view_func=ResultsPage.as_view('results_page'))
+app.add_url_rule('/bill_form_page', view_func=BillFormPage.as_view('bill_form_page'))
+# app.add_url_rule('/results', view_func=ResultsPage.as_view('results_page'))  RESULTS NOW ON "bill_form_page.html
 
 app.run(debug=True)
